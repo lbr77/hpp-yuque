b66b52b6da966b0cfa80f2ef2371a994260aa108
Steve Li <2298454647@qq.com>
2021-03-20 16:03:45 +0800

    QAQWQ

--- index.py
+++ index.py
@@ -2,9 +2,11 @@ from requests import *
 from os import environ, walk, path, listdir
 from hashlib import md5
 from yaml import load, warnings
-
 warnings({'YAMLLoadWarning': False})
 yuquetoken = environ.get("YUQUE_TOKEN");
+if yuquetoken == None:
+    print("未定义yuque_token")
+    exit(1)
 # load Configurations
 config = {};
 with open("_config.yml", "r") as f:
@@ -13,7 +15,6 @@ with open("_config.yml", "r") as f:
 print("配置读取成功")
 
 def loadfiledir(root):
-    # print(root)
     fileq = []
     for root, dirs, files in walk(root):
         for file in files: 
@@ -24,12 +25,13 @@ def loadfiles():
     articles = [];
     n = 0
     for file in loadfiledir(config['postPath']):
-        with open(file,"r",encoding = 'gb18030',errors='ignore') as f:
+        with open(file,"r",encoding = 'UTF-8',errors='ignore') as f:
             fileqwq=f.read()
             articles.append({
-                "title": file.split('\\')[-1],
+                "title": file.split('\\')[-1].split('.')[0],
                 "data": fileqwq,
-                "slug": md5(fileqwq.encode('utf8')).hexdigest()[0:5]
+                "slug": md5(fileqwq.encode('utf8')).hexdigest()[0:5],
+                "id": str(n)
             });
             f.close();
         n = n+1
@@ -46,6 +48,7 @@ def uploadfiles(filename,file,slug):
         "X-Auth-Token": yuquetoken,
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/91.0.4435.0 Safari/537.36 Edg/91.0.825.0"
     })
+
 print("开始读取文件")
 
 for i in loadfiles():
